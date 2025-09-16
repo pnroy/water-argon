fe20a818e1f689f607c5a6beed9fb58cbbca4f1a
pnroy <pnroy@uwaterloo.ca>
2025-09-16 13:54:08 -0400

    updated lattice code to place dopant at the center

--- lattice/lattice.py
+++ lattice/lattice.py
@@ -7,24 +7,53 @@ from openmmtools import testsystems
 import mdtraj as md
 import numpy as np
 
-N=108
-m=3
+m=4
 N=4*m**3
-test_sys = testsystems.LennardJonesFluid(nparticles=108, lattice=True,reduced_density=0.5)
+test_sys = testsystems.LennardJonesFluid(nparticles=N, lattice=True,reduced_density=0.5)
 system, positions = test_sys.system, test_sys.positions
 
 
-r=3.72
+r=3.755
 
 f=open('lattice.xyz','w')
 f.write(str(N-1)+'\n ')
 f.write(' '+'\n')
 
+
+COM=np.zeros(3,float)
+
+R=np.zeros((N,3),float)
 for i in range(N):
-    if i!=int((N-1)/2):
+    for a in range(3):
+        COM[a]+=positions[i][a]._value
+COM=(1.0/N)*COM
+
+for i in range(N):
+    for a in range(3):
+        R[i][a]=positions[i][a]._value-COM[a]
+
+index_vac=0
+
+d_origin=10.0
+for i in range(N):
+    d=0.0
+    for a in range(3):
+        d+=R[i][a]**2
+    d=np.sqrt(d)
+    if d<d_origin:
+        index_vac=i
+        d_origin=d
+
+dist=0.0
+for a in range(3):
+    dist+=(R[1][a]-R[0][a])**2
+dist=np.sqrt(dist)
+
+for i in range(N):
+    if i!=int(index_vac):
         f.write('Ar ')
         for a in range(3):
-            f.write(str(r*((positions[i][a]._value-positions[int((N-1)/2)][a]._value))/positions[1][1]._value/np.sqrt(2))+' ')
+            f.write(str(r*((R[i][a]-R[int(index_vac)][a]))/dist/np.sqrt(2))+' ')
         f.write('\n')
 
 #integrator = mm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds,1.0*unit.femtoseconds)
